5ee23fdc9753ec737668597b167f2b8512606098
IlyaZaremba <45465500+IlyaZaremba@users.noreply.github.com>
2021-08-31 16:11:07 +0300

    Update views.py

--- kizaru/views.py
+++ kizaru/views.py
@@ -10,18 +10,12 @@ def index(request):
     if request.method == 'POST':
         form = InputForm(request.POST)
         if form.is_valid():
-            # Take data from the initial form
             name_input = form.cleaned_data['name0']
-            # Convert to json type
             data = json.dumps(name_input, ensure_ascii=False)
-            # Create object
             InputModel.objects.create(data=data, field='name0')
-
-            # Loop take data from other form
             for count in range(1, len(request.POST) - 1):
                 query = 'name' + str(count)
                 input_value = request.POST[query]
-                # Check for empty field
                 if input_value:
                     data = json.dumps(input_value, ensure_ascii=False)
                     field = json.dumps(query)
@@ -34,7 +28,6 @@ def index(request):
 
 
 def done(request):
-    # Take data from the db
     query = InputModel.objects.all().values('id', 'field', 'data')
     json_list = list(query)
     return render(request, 'kizaru/base.html', {'json_list': json_list})
